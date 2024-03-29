3fdbb78b71b7790780734135a10256b2778d0371
yeomyaloo <yeom456@gmail.com>
2024-03-29 16:34:02 +0900

    Rename: JPA 레포지토리 엔티티 타입 변경

--- src/main/java/imyeom_lck/predict/persistence/jpa/PredictRepository.java
+++ src/main/java/imyeom_lck/predict/persistence/jpa/PredictRepository.java
@@ -1,9 +1,11 @@
 package imyeom_lck.predict.persistence.jpa;
 
 import imyeom_lck.member.domain.entity.Member;
+import imyeom_lck.predict.domain.entity.Predict;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PredictRepository extends JpaRepository<PredictRepository, Long> {
+public interface PredictRepository extends JpaRepository<Predict, Long> {
 
 
 }
