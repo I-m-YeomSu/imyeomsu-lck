e7638350bf8a98a4e0e97cf4c264aed64f79b8b0
Yuri Lim <vnm0774@naver.com>
2024-04-02 09:22:20 +0900

    Fix: DummyMember를 builder 패턴으로 수정

--- src/test/java/imyeom_lck/member/dummy/DummyMember.java
+++ src/test/java/imyeom_lck/member/dummy/DummyMember.java
@@ -5,11 +5,11 @@ import imyeom_lck.member.domain.entity.Member;
 public class DummyMember {
 
     public static Member createDummyMember(String memberName, String password, String phoneNumber, String loginId) {
-        Member member = new Member();
-        member.setName(memberName);
-        member.setPassword(password);
-        member.setPhoneNumber(phoneNumber);
-        member.setLoginId(loginId);
-        return member;
+        return Member.builder()
+                .name(memberName)
+                .password(password)
+                .phoneNumber(phoneNumber)
+                .loginId(loginId)
+                .build();
     }
 }
