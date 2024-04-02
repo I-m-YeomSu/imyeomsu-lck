53efa543368fff450ab0667feb4612d179fe27f4
popododo0720 <popododo0720@naver.com>
2024-04-02 09:16:46 +0900

    feat: test

---
+++ src/main/java/imyeom_lck/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package imyeom_lck;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/swagger/**"};
+
+        return GroupedOpenApi.builder()
+                .group("COUPLE API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+
+}
