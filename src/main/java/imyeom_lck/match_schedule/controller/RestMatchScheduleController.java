4c51f57317516579bcc123187b8e3510364b751d
hwiju yeom <yeom456@gmail.com>
2024-04-06 19:43:15 +0900

    Refactor: 컨벤션에 맞게 코드 수정 작업 진행

--- src/main/java/imyeom_lck/match_schedule/controller/RestMatchScheduleController.java
+++ src/main/java/imyeom_lck/match_schedule/controller/RestMatchScheduleController.java
@@ -1,18 +1,12 @@
 package imyeom_lck.match_schedule.controller;
 
-
-
 import imyeom_lck.match_schedule.domain.dto.MatchesResponseDTO;
 import imyeom_lck.match_schedule.domain.dto.NextMatchResponseDTO;
 import imyeom_lck.match_schedule.service.inter.MatchScheduleService;
-import imyeom_lck.member.domain.dto.MemberDetailsResponseDTO;
-import imyeom_lck.member.service.inter.MemberService;
 import imyeomsu.lck.common_utils.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,28 +18,26 @@ public class RestMatchScheduleController {
     private final MatchScheduleService matchScheduleService;
 
     @GetMapping(value="/matches")
-    public ResponseEntity<ResponseDto<List<MatchesResponseDTO>>> getAllMatches() {
+    public ResponseDto<List<MatchesResponseDTO>> getAllMatches() {
         List<MatchesResponseDTO> matchesResponseDTO = matchScheduleService.getAllMatches();
 
-        ResponseDto<List<MatchesResponseDTO>> response = ResponseDto.<List<MatchesResponseDTO>>builder()
+        return ResponseDto.<List<MatchesResponseDTO>>builder()
                 .data(matchesResponseDTO)
                 .status(HttpStatus.OK)
                 .success(true)
                 .build();
 
-        return ResponseEntity.ok(response);
+
     }
 
     @GetMapping(value = "/matches/nextMatch")
-    public ResponseEntity<ResponseDto<NextMatchResponseDTO>> getNextMatch() {
+    public ResponseDto<NextMatchResponseDTO> getNextMatch() {
         NextMatchResponseDTO nextMatchResponseDTO = matchScheduleService.getNextMatch();
 
-        ResponseDto<NextMatchResponseDTO> response = ResponseDto.<NextMatchResponseDTO>builder()
+        return ResponseDto.<NextMatchResponseDTO>builder()
                 .data(nextMatchResponseDTO)
                 .status(HttpStatus.OK)
                 .success(true)
                 .build();
-
-        return ResponseEntity.ok(response);
     }
 }
