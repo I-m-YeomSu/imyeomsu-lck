8263d9306b45ae167ad789064966ce71e6c29eab
hwiju yeom <yeom456@gmail.com>
2024-04-07 18:28:47 +0900

    Test: 뷰를 반환하는 엔드 포인트 테스트 작업 진행

--- src/test/java/imyeom_lck/common/controller/IndexControllerTest.java
+++ src/test/java/imyeom_lck/common/controller/IndexControllerTest.java
@@ -2,13 +2,10 @@ package imyeom_lck.common.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,7 +13,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @WebMvcTest(IndexController.class)
-@AutoConfigureMockMvc
 class IndexControllerTest {
 
     @Autowired
@@ -29,6 +25,7 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("메인 화면으로 가는 컨트롤러 테스트 - 성공")
     void indexForm() throws Exception {
         this.mockMvc.perform(get("/"))
                 .andDo(print())
@@ -37,6 +34,7 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("메인 이벤트 화면으로 가는 컨트롤러 테스트 - 성공")
     void mainEventForm() throws Exception {
         this.mockMvc.perform(get("/event/main"))
                 .andDo(print())
