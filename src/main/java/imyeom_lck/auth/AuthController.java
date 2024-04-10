6465006382f6fb00dbf151421cf61425bd4fd218
hwiju yeom <yeom456@gmail.com>
2024-04-10 14:48:40 +0900

    Update AuthController.java

--- src/main/java/imyeom_lck/auth/AuthController.java
+++ src/main/java/imyeom_lck/auth/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AuthController {
 	@GetMapping("/my-profile")
 	public String profileForm(){
+		// 후에 로그인 회원 관련해서 로그인 하지 않은 회원은 로그인 화면으로 이동하게 하고 로그인 회원에겐 회원 정보를 노출시킬 예정입니다.
 		return "fragments/auth/my-profile";
 
 	}
