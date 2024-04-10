2e7bda5a19cc58f3bf6e9359a5afb280ce01c646
hwiju yeom <yeom456@gmail.com>
2024-04-10 14:43:18 +0900

    Refactor: RedisTemplate은 동일하게 가져가야 하기 때문에 코드 수정

--- src/main/java/imyeom_lck/config/redis/RedisConfig.java
+++ src/main/java/imyeom_lck/config/redis/RedisConfig.java
@@ -1,10 +1,16 @@
 package imyeom_lck.config.redis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
@@ -21,7 +27,17 @@ public class RedisConfig {
         redisStandaloneConfiguration.setPort(port);
         redisStandaloneConfiguration.setDatabase(dbIndex);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+    @Bean
+    public RedisTemplate<?, ?> matchRankingRedisTemplate(ObjectMapper objectMapper,RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
 
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
     }
 }
 
