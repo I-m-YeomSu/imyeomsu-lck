18e5118bdd50cfcd189c977ad1f8de29dec55665
hwiju yeom <yeom456@gmail.com>
2024-04-10 14:36:39 +0900

    Feat: 최상위 레디스 설정 파일 작성

---
+++ src/main/java/imyeom_lck/config/redis/RedisConfig.java
@@ -0,0 +1,27 @@
+package imyeom_lck.config.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    public RedisConnectionFactory redisConnectionFactory(int dbIndex) {
+        final RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setDatabase(dbIndex);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+
+    }
+}
+
