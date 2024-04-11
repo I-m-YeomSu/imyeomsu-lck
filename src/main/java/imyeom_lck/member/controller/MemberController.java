3cb7108bfac4c7fcfc9ce1f9149254f20e52cc1d
popododo0720 <popododo0720@naver.com>
2024-04-11 11:25:14 +0900

    Feat: memberview

--- src/main/java/imyeom_lck/member/controller/MemberController.java
+++ src/main/java/imyeom_lck/member/controller/MemberController.java
@@ -1,14 +1,20 @@
 package imyeom_lck.member.controller;
 
 
+import imyeom_lck.member.domain.dto.MemberDetailsResponseDTO;
+import imyeom_lck.member.service.inter.MemberService;
+import imyeomsu.lck.common_utils.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/members")
 public class MemberController {
-
 
 
 }
