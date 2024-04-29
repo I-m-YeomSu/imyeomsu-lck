17a3f511ab50cb6454c4327b4cc08c535e0f7f67
hwiju yeom <yeom456@gmail.com>
2024-04-29 23:01:18 +0900

    Feat: 시큐리티 설정 작업 진행

--- src/main/java/imyeom_lck/config/SecurityConfig.java
+++ src/main/java/imyeom_lck/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package imyeom_lck.config;
 
-import org.apache.catalina.filters.CorsFilter;
+
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,15 +12,20 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import imyeom_lck.auth.filter.CustomerAuthenticationFilter;
+import imyeom_lck.auth.filter.CustomAuthenticationFilter;
 import imyeom_lck.auth.provider.CustomAuthenticationProvider;
 import imyeom_lck.auth.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.logging.Filter;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @RequiredArgsConstructor
@@ -27,21 +34,24 @@ public class SecurityConfig {
 
 	private final CustomUserDetailsService customUserDetailsService;
 
+	private final CorsConfig corsConfig;
+
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-		http.cors(corsConfigurer -> corsConfigurer.disable());
+		http
+			.addFilter(corsConfig.corsFilter())
+			.addFilterAt(authenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
 		http.csrf(csrfConfigurer -> csrfConfigurer.disable());
 
 		http.headers(headerConfig -> headerConfig.frameOptions(
 			frameOptionsConfig -> frameOptionsConfig.disable()));
 
-		http.addFilter(new CorsFilter());
 
 		http.formLogin(httpSecurityFormLoginConfigurer -> {
-			httpSecurityFormLoginConfigurer.loginPage("/auth/login");
+			httpSecurityFormLoginConfigurer.loginPage("/auth/login").permitAll();
 			httpSecurityFormLoginConfigurer.passwordParameter("password");
 			httpSecurityFormLoginConfigurer.usernameParameter("loginId");
 		});
@@ -63,18 +73,19 @@ public class SecurityConfig {
 			logout.deleteCookies("JSESSIONID", "remember-me");
 		});
 
-		http.addFilterAt(authenticationFilter(),UsernamePasswordAuthenticationFilter.class);
-
 
 		//사용자 권한이 필요한 곳엔 로그인 작업을 할수 있게 합니다.
-		http.authorizeHttpRequests(auth ->{
+		http.authorizeHttpRequests(auth -> {
+
+			auth.requestMatchers(antMatcher("/auth/**")).permitAll();
+			auth.requestMatchers(antMatcher("/apply/**")).authenticated();
+			auth.requestMatchers(antMatcher("comments/**")).authenticated();
+			auth.requestMatchers(antMatcher("/api/predict/vote")).authenticated();
+			auth.requestMatchers(antMatcher("/admin/**")).hasAuthority("ROLE_ADMIN"); // 관리자만 접근 가능하게 합니다.
+			auth.requestMatchers(antMatcher("/members/**")).hasAuthority("ROLE_USER");
 			auth.anyRequest().permitAll();
-			auth.requestMatchers("/apply/**", "comments/**", "/api/predict/vote", "").authenticated();
-			auth.requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN"); // 관리자만 접근 가능하게 합니다.
-			auth.requestMatchers("/members/**").hasAuthority("ROLE_USER");
-			auth.requestMatchers("/admin/**").hasRole("ADMIN"); // 관리자만 접근 가능하게 합니다.
-			auth.requestMatchers("/members/**").hasRole("USER");
 		});
+
 
 		return http.build();
 
@@ -82,8 +93,8 @@ public class SecurityConfig {
 
 
 	@Bean
-	public CustomerAuthenticationFilter authenticationFilter() throws Exception {
-		CustomerAuthenticationFilter customAuthenticationFilter = new CustomerAuthenticationFilter(authenticationManager(null));
+	public CustomAuthenticationFilter authenticationFilter() throws Exception {
+		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager(null));
 
 		// 해당 부분은 Custom한 AuthenticationManager를 사용해도 되지만 굳이 사용하지 않아도 되기 떄문에 설정을 이렇게 진행 해줌
 		customAuthenticationFilter.setAuthenticationManager(new ProviderManager(customAuthenticationProvider()));
@@ -102,6 +113,7 @@ public class SecurityConfig {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
 
+
 	/**
 	 * 정적 리소스와 관련된 정보를 security 대상에서 제외시킬 때 사용합니다.
 	 * */
@@ -115,6 +127,7 @@ public class SecurityConfig {
 				);
 		};
 	}
+
 
 
 	//사용자 계정 생성 시 해당 엔코더를 사용해 암호화한 데이터를 저장하고 로그인 시 해당 엔코더를 사용해 비밀번호를 확인합니다.
